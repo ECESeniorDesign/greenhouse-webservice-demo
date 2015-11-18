569c7d1fd6ccb131d99ceda85b82e795a327c090
Chase Conklin <chase@conklins.net>
2015-11-18 10:27:14 -0500

    added socket io

--- app.py
+++ app.py
@@ -1,5 +1,42 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 app = Flask(__name__)
+import eventlet
+eventlet.monkey_patch()
+from flask_socketio import SocketIO
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode='eventlet')
+import time
+from threading import Thread
+thread = None
+import random
+
+ideal_water = 45
+water_tolerance = 5
+ideal_sun = 30
+sun_tolerance = 10
+ideal_pH = 7
+pH_tolerance = 0.2
+ideal_humidity = 0.72
+humidity_tolerance = 0.07
+
+def background_thread():
+    while True:
+        time.sleep(10)
+        global sun, water, pH, humidity
+        sun += random.randint(-5, 5)
+        water += random.randint(-5, 5)
+        humidity = random.random()
+        with app.test_request_context('/your-plant'):
+            new_template = render_template('vitals.html',
+                sun_bar=SunBar(sun, ideal_sun, sun_tolerance),
+                water_bar=WaterBar(water, ideal_water, water_tolerance),
+                pH=VitalInfo("pH", pH, ideal_pH, pH_tolerance, "0.1f",
+                             pH_correction),
+                humidity=VitalInfo("Humidity", humidity, ideal_humidity,
+                                   humidity_tolerance, "0.1%", lambda *_: None))
+        socketio.emit('new-data', {
+            'new-page': new_template
+        })
 
 @app.route("/")
 def home():
@@ -7,27 +44,30 @@ def home():
 
 @app.route("/your-plant/")
 def your_plant():
-    remaining = float(request.args.get("remaining", 85))
-    total = float(request.args.get("total", 100))
-    sun=int(request.args.get("sun", 40))
-    water=int(request.args.get("water", 35))
-    pH = float(request.args.get("pH", 8.1))
-    humidity = float(request.args.get("humidity", 0.67))
-    ideal_water = 45
-    water_tolerance = 5
-    ideal_sun = 30
-    sun_tolerance = 10
-    ideal_pH = 7
-    pH_tolerance = 0.2
-    ideal_humidity = 0.72
-    humidity_tolerance = 0.07
+    global sun, water, pH, humidity, remaining, total
+
+    remaining = float(session.get("remaining", 85))
+    total = float(session.get("total", 100))
+    sun = int(session.get("sun", 40))
+    water = int(session.get("water", 35))
+    pH = float(session.get("pH", 8.1))
+    humidity = float(session.get("humidity", 0.67))
+
+    global thread
+    if thread is None:
+        thread = Thread(target=background_thread)
+        thread.daemon = True
+        thread.start()
+
     assert total > remaining
     return render_template('your_plant.html',
         sun_bar=SunBar(sun, ideal_sun, sun_tolerance),
         water_bar=WaterBar(water, ideal_water, water_tolerance),
         maturity_dial=MaturityDial(remaining, total),
-        pH=VitalInfo("pH", pH, ideal_pH, pH_tolerance, "0.1f", pH_corrective_action),
-        humidity=VitalInfo("Humidity", humidity, ideal_humidity, humidity_tolerance, "0.1%", lambda *_: None))
+        pH=VitalInfo("pH", pH, ideal_pH, pH_tolerance, "0.1f",
+                     pH_correction),
+        humidity=VitalInfo("Humidity", humidity, ideal_humidity,
+                           humidity_tolerance, "0.1%", lambda *_: None))
 
 
 class MaturityDial(object):
@@ -41,10 +81,10 @@ class BaseBar(object):
 
     def __init__(self, current, ideal, tolerance):
         # normalize values so that ideal is 50%
-        self.current = ((float(current) * 50) / ideal) 
+        self.current = ((float(current) * 50) / ideal)
         self.ideal = 50
         self.tolerance = ((float(tolerance) * 50) / ideal)
-        
+
         self.within_tolerance = tolerance >= abs(ideal - current)
         self.over_ideal = self.current > self.ideal
         if self.current > self.ideal + self.tolerance:
@@ -71,7 +111,7 @@ class WaterBar(BaseBar):
     icon = "wi wi-raindrop"
 
 class VitalInfo(object):
-    def __init__(self, name, current, ideal, tolerance, format, corrective_action):
+    def __init__(self, name, current, ideal, tolerance, format, correction):
         self.name = name
         # The 0.001 is to account for floating issues where effectively equal numbers don't compare as expected
         self.within_tolerance = tolerance >= abs(ideal - current) - 0.001
@@ -79,11 +119,11 @@ class VitalInfo(object):
         self.formatted_ideal_value = "{0:{format}}".format(ideal, format=format)
         self.formatted_tolerance = "{0:{format}}".format(tolerance, format=format)
         # If there is no corrective action to suggest, don't say anything
-        self.corrective_action = corrective_action(current, ideal, tolerance) or ""
+        self.correction = correction(current, ideal, tolerance) or ""
 
-def pH_corrective_action(current, ideal, tolerance):
+def pH_correction(current, ideal, tolerance):
     if current > ideal:
         return "Add a teaspoon of white vinegar to the base of the plant."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
--- static/js/your_plant.js
+++ static/js/your_plant.js
@@ -1,4 +1,26 @@
+function set_popover(states, elem_id) {
+  $('#' + elem_id).popover();
+  $('#' + elem_id).on('show.bs.popover', function() {
+    states[elem_id] = true;
+  });
+  $('#' + elem_id).on('hide.bs.popover', function() {
+    states[elem_id] = false;
+  });
+  if (states[elem_id]) {
+    $('#' + elem_id).popover("show");
+  }
+}
+
 $(function() {
-    $(".dial").knob();
-    $('[data-toggle="popover"]').popover()
+  var states = {"Humidity" : false, "pH" : false}
+  $(".dial").knob();
+  set_popover(states, "Humidity");
+  set_popover(states, "pH");
+  namespace = "";
+  var socket = io.connect('http://' + document.domain + ':' + location.port + namespace);
+  socket.on("new-data", function(stat) {
+    $("#vitals").html(stat["new-page"]);
+    set_popover(states, "Humidity");
+    set_popover(states, "pH");
+  });
 });
