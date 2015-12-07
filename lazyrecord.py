52c0d3789e92425f6db353b5ab7c41fce691271a
Chase Conklin <chase@conklins.net>
2015-12-07 17:14:55 -0500

    enhanced lazyrecord

--- lazyrecord.py
+++ lazyrecord.py
@@ -11,13 +11,20 @@ class Base(object):
             if attr in (self.__class__.__attributes__ + ["created_at"]):
                 setattr(self, attr, val)
         self.id = None
-    
+
+    @classmethod
+    def backend(cls, **kwargs):
+        obj = cls()
+        for attr, val in kwargs.items():
+            setattr(obj, attr, val)
+        return obj
+
     def save(self):
         if self.id:
             update_command_arg = ", ".join("{} = ?".format(attr) for attr in self.__class__.__attributes__)
             cmd = "update {table} set {update_command_arg} where {table}.id = ?".format(
                 update_command_arg=update_command_arg,
-                table=self.__class__.__name__.lower() + "s")
+                table=self.__class__.table)
             x = g.db.execute(cmd,
                 [getattr(self, attr) for attr in self.__class__.__attributes__] + [self.id])
             g.db.commit()
@@ -26,7 +33,7 @@ class Base(object):
             cmd = "insert into {table} ({}) values ({})".format(
                 ", ".join(self.__class__.__attributes__ + ["created_at"]),
                 ", ".join(["?"] * (len(self.__class__.__attributes__) + 1)),
-                table=self.__class__.__name__.lower() + "s")
+                table=self.__class__.table)
             x = g.db.execute(cmd,
                 [getattr(self, attr) for attr in self.__class__.__attributes__] + [created_at])
             self.id = int(x.lastrowid)
@@ -35,30 +42,54 @@ class Base(object):
 
     @classmethod
     def find(cls, id):
-        cmd = 'select {attrs}, created_at from {table} where {table}.id = ?'.format(
-            table = cls.__name__.lower() + "s",
+        cmd = 'select {attrs}, created_at, id from {table} where {table}.id = ?'.format(
+            table = cls.table,
             attrs = ", ".join(cls.__attributes__)
         )
         cur = g.db.execute(cmd, [id])
-        result = cur.fetchall()
+        result = cur.fetchone()
         if result:
-            args = dict(zip(cls.__attributes__ + ["created_at"], result[0]))
-            obj = cls(**args)
-            obj.id = id
+            args = dict(zip(cls.__attributes__ + ["created_at", "id"], result))
+            obj = cls.backend(**args)
             return obj
         else:
-            raise RecordNotFound(id)
+            raise RecordNotFound({'id': id})
+
+    @classmethod
+    def find_by(cls, **kwargs):
+        args = list(kwargs)
+        cmd = 'select {attrs}, created_at, id from {table} where {where_clause}'.format(
+            table = cls.table,
+            attrs = ", ".join(cls.__attributes__),
+            where_clause = " and ".join([
+                "{table}.{attr} = ?".format(table = cls.table, attr = attr)
+                for attr in args]))
+        cur = g.db.execute(cmd, [kwargs[attr] for attr in args])
+        result = cur.fetchone()
+        if result:
+            args = dict(zip(cls.__attributes__ + ["created_at", "id"], result))
+            obj = cls.backend(**args)
+            return obj
+        else:
+            raise RecordNotFound(kwargs)
 
     @classmethod
     def all(cls):
         def create_objects(cur):
             for record in cur.fetchall():
                 args = dict(zip(cls.__attributes__ + ["id", "created_at"], record))
-                obj = cls(**args)
-                obj.id = int(args["id"])
+                obj = cls.backend(**args)
                 yield obj
         cmd = 'select {attrs}, id, created_at from {table}'.format(
-            table = cls.__name__.lower() + "s",
+            table = cls.table,
             attrs = ", ".join(cls.__attributes__)
         )
         return list(create_objects(g.db.execute(cmd)))
+
+    class __metaclass__(type):
+        @property
+        def table(self):
+            return self.__name__.lower() + "s"
+
+    def __repr__(self):
+        return "Plant({})".format(", ".join("{}={!r}".format(attr, getattr(self, attr)) for attr in self.__class__.__attributes__))
--- plant.py
+++ plant.py
@@ -6,3 +6,11 @@ class Plant(lazyrecord.Base):
         "light_tolerance", "acidity_ideal", "acidity_tolerance", "humidity_ideal",
         "humidity_tolerance", "mature_on"
     ]
+
+    @classmethod
+    def for_slot(Plant, slot_id):
+        try:
+            return Plant.find_by(slot_id=slot_id)
+        except lazyrecord.RecordNotFound:
+            # Probably want a better sentinel object
+            return None
